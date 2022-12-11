d5c3bb487cf38d6d84efe8c02ccba85fe597a2ed
Mohammad Miras Ahmadi <94456884+Mohmmad-Miras@users.noreply.github.com>
2022-12-11 11:28:23 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,3 @@ import { getFromCacheOrApi } from "Base"
 export default function Home() {
   return <div>Home page</div>
 }
-
-export async function getServerSideProps(context) {
-    
-  const data = await getFromCacheOrApi("/v1/01925c4c-b71b-46f5-ba9a-522071071374")
-
-    return {
-        props: {
-            ...data,
-        },
-    }
-}
