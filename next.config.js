b8f15c4cf8c691604903a33e649752c1912b4efe
Mohammad Miras Ahmadi <94456884+Mohmmad-Miras@users.noreply.github.com>
2022-12-11 11:30:29 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  middleware: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false }
-    config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
-    })
-    return config;
-}
-}
-
-module.exports = nextConfig
+const configNext = require('./next')
+module.exports = configNext({
+  storage: 'storage.amlakmosallas.com'
+})
