929e8a2804a4c5d7e34fcb940e87717c6d18d63e
Mohammad Miras Ahmadi <94456884+Mohmmad-Miras@users.noreply.github.com>
2022-12-11 11:27:48 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import { getFromCacheOrApi } from "Base"
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>     
-    </div>
-  )
+  return <div>Home page</div>
 }
 
 export async function getServerSideProps(context) {
